35d6cb10b51f3d6d1b8a38e73af92bc38681c0da
Imad <imadarchid@gmail.com>
2020-04-28 00:13:17 +0000

    sorting + fixing issue with weekly updating

--- exportMongo.py
+++ exportMongo.py
@@ -10,20 +10,20 @@ connect("opcvm")
 class Funds(Document):
     FREQUENCY = StringField(required=True)
     ISIN = StringField(unique=True, required=True)
-    AMC = IntField(required=False)
-    MCCODE = IntField(required=False)
+    AMC = FloatField(required=False)
+    MCCODE = FloatField(required=False)
     NAME = StringField(required=True)
-    MANAGER = StringField(required=True)
-    LEGAL_TYPE = StringField(required=True)
-    INV_TYPE = StringField(required=True)
-    SUBSCRIBERS = StringField(required=True)
-    RESULT_TYPE = StringField(required=True)
+    MANAGER = StringField(required=False)
+    LEGAL_TYPE = StringField(required=False)
+    INV_TYPE = StringField(required=False)
+    SUBSCRIBERS = StringField(required=False)
+    RESULT_TYPE = StringField(required=False)
     DEPOSITORS = StringField(required=False)
     PLACEMENT_NETWORK = StringField(required=False)
     PROMOTERS = StringField(required=False)
-    ENTRY_RATE = FloatField(required=True)
-    EXIT_RATE = FloatField(required=True)
-    MGT_RATE = FloatField(required=True)
+    ENTRY_RATE = FloatField(required=False)
+    EXIT_RATE = FloatField(required=False)
+    MGT_RATE = FloatField(required=False)
 
     meta = {'strict': False}
 
@@ -38,6 +38,7 @@ weekly_folder = Path('data_files', 'weekly/')
 
 def daily():
     files = daily_folder.glob('**/*')
+    files = sorted(files, reverse=True)
     for f in files:
         try:
             excel_file = pandas.read_excel(f, sheet_name=0)
@@ -63,7 +64,7 @@ def daily():
                         ENTRY_RATE=entry['Droits  Entr\u00e9es'], EXIT_RATE=entry['Droits Sorties'], MGT_RATE=entry['Frais de gestion'])
                         opcvm.save()
                         print("saved " + entry['CODE ISIN'])
-                    except Exception as f:
+                    except KeyError as f:
                         print(f)
                 except ValidationError as e:
                     print(e)
@@ -76,37 +77,30 @@ def daily():
             
 def weekly():
     files = weekly_folder.glob('**/*')
+    files = sorted(files, reverse=True)
     for f in files:
         try:
+            print(f.name)
             excel_file = pandas.read_excel(f, sheet_name=0)
             json_str = excel_file.to_json(orient='records')
             entries = json.loads(json_str)
             for entry in entries:
                 try:
-                    print("processing ")
-                    opcvm = Funds(FREQUENCY="weekly", MCCODE=entry['Code Maroclear'], ISIN=entry['CODE ISIN'], NAME=entry['D\u00e9nomination OPCVM'], 
-                    MANAGER=entry['Soci\u00e9t\u00e9 de Gestion'], LEGAL_TYPE=entry['Nature juridique'], INV_TYPE=entry['Classification'], 
-                    SUBSCRIBERS=entry['Souscripteurs'], RESULT_TYPE=entry['Affectation des r\u00e9sultats'], DEPOSITORS=entry['D\u00e9positaire'], 
-                    PLACEMENT_NETWORK=entry['R\u00e9seau placeur'], PROMOTERS=entry['Promoteurs'], ENTRY_RATE=entry['Commission de souscription'],
-                    EXIT_RATE=entry[' Commission de rachat'], MGT_RATE=entry['Frais de gestion'])
+                    opcvm = Funds(FREQUENCY="weekly", ISIN=entry['CODE ISIN'], MCCODE=entry['Code Maroclear'], NAME=entry['D\u00e9nomination OPCVM'], 
+                            MANAGER=entry['Soci\u00e9t\u00e9 de Gestion'], LEGAL_TYPE=entry['Nature juridique'], SUBSCRIBERS=entry['Souscripteurs'],
+                            INV_TYPE=entry['Classification'], DEPOSITORS=entry['D\u00e9positaire'], RESULT_TYPE=entry['Affectation des r\u00e9sultats'], PLACEMENT_NETWORK=entry['R\u00e9seau placeur'],
+                            ENTRY_RATE=entry['Commission de souscription'], EXIT_RATE=entry[' Commission de rachat'], MGT_RATE=entry['Frais de gestion'])
                     opcvm.save()
-                    print("saved " + entry['CODE ISIN'])
-                except KeyError as e:
-                    print(e)
-                    try:
-                        opcvm = Funds(FREQUENCY="weekly", ISIN=entry['CODE ISIN'], AMC=entry['AMC'], NAME=entry['OPCVM'], 
-                        MANAGER=entry['GESTIONNAIRE'], LEGAL_TYPE=entry['FORME'], SUBSCRIBERS=entry['SOUSCRIPTEURS'],
-                        INV_TYPE=entry['CATEGORIE'], RESULT_TYPE=entry['Affectation des r\u00e9sultats'], 
-                        PLACEMENT_NETWORK=entry['RESEAU PLACEUR'], ENTRY_RATE=entry['Droits  Entr\u00e9es'], EXIT_RATE=entry['Droits Sorties'], 
-                        MGT_RATE=entry['Frais de gestion'])
-                        opcvm.save()
-                        print("saved " + entry['CODE ISIN'])
-                    except Exception as f:
-                        print(f)
-                except ValidationError as e:
-                    print(e)
-                except NotUniqueError as j:
-                    print("already exists")
+                except KeyError as k:
+                    opcvm = Funds(FREQUENCY="weekly", ISIN=entry['CODE ISIN'], AMC=entry['AMC'], NAME=entry['OPCVM'], 
+                            MANAGER=entry['GESTIONNAIRE'], LEGAL_TYPE=entry['NATURE JURIDIQUE'], SUBSCRIBERS=entry['SOUSCRIPTEUR'],
+                            INV_TYPE=entry['CLASSIFICATION'], RESULT_TYPE=entry['Affectation du r\u00e9sultat'], PLACEMENT_NETWORK=entry['COMMERCIALISATEUR'],
+                            ENTRY_RATE=entry['Commission Souscription'], EXIT_RATE=entry['Commission Rachat'], MGT_RATE=entry['Frais Gestion'])
+                    opcvm.save()
+                except ValidationError as v:
+                    print(v)
+                except NotUniqueError as n:
+                    print(n)
         except:
             print("error reading the excel file")
     print("Finished processing all OPCVMs with weekly frequency") 
